06764dd2e932cc4dd9aa0019be4daa9b37a75576
Ludovico Zitelli <ludovico.zitelli@gmail.com>
2022-09-03 12:27:36 +0200

    Updated Privacy Policy, added CNAME file to build

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -10,7 +10,19 @@ export class PrivacyPolicyComponent implements OnInit {
 
   constructor(private location: Location) { }
 
+  private loadCookieDeclaration(url: string) {
+    const target = document.getElementById("cookie-policy")
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.async = true;
+    script.id = 'CookieDeclaration'
+    script.src = url;
+    script.type = 'text/javascript'
+    target.after(script);
+  }
+
   ngOnInit(): void {
+    this.loadCookieDeclaration("https://consent.cookiebot.com/f8a8cfd2-0b9d-4cf5-b9e6-9fb4aa53441c/cd.js")
   }
 
   goBack(): void {
